e67cbe68a7ebc99729fa68b88f81ca7e525d510d
Dmitry <215505940+dima-bond-git@users.noreply.github.com>
2025-10-15 16:45:03 +0700

    fixed paths for astro2

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -15,6 +15,7 @@ export default defineConfig({
       alias: {
         '@': path.resolve(__dirname, './src'),
       },
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
     },
   },
 });
